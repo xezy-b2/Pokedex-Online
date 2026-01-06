464cb56b30579fb0a312afd0bc2469db39f85e9d
xezy-b2 <xezy_4l@outlook.fr>
2026-01-06 16:24:01 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
