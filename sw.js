08a94221f05bbe8b53dbe2bbe463f832cd2c2238
xezy-b2 <xezy_4l@outlook.fr>
2026-01-06 16:26:55 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
