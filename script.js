77cf2828ca124313f1fe218ea1597f1c197c91df
xezy-b2 <xezy_4l@outlook.fr>
2026-01-16 13:12:25 +0100

    Update script.js

--- script.js
+++ script.js
@@ -206,7 +206,7 @@ async function loadProfile() {
                     <div class="ball-item"><img src="${BALL_URL}safari-ball.png"><br><b>x${user.safariballs || 0}</b><br><small>Safari ball</small></div>
                     <div class="ball-item">
                         <img src="https://raw.githubusercontent.com/xezy-b2/Pokedex-Online/refs/heads/main/elbaball30retesttt.png" style="filter: hue-rotate(290deg) brightness(1.3);">
-                        <br><b>x${user.ellbaballs || 0}</b>
+                        <br><b>x${user.elbaballs || 0}</b>
                         <br><small style="font-size:0.8em;">Ellba Ball</small>
                 </div>
             </div>
@@ -304,9 +304,3 @@ async function buyItem(key, qty) {
 
 function logout() { localStorage.clear(); location.reload(); }
 document.addEventListener('DOMContentLoaded', initializeApp);
-
-
-
-
-
-
